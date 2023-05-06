451cc7238086ed62c4c8c99da194528dd3cf6b0b
AndreyPysarenko <114475908+AndreyPysarenko@users.noreply.github.com>
2023-05-06 20:32:40 +0300

    update with Prettier swiper.js file

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,25 +1,29 @@
-const swiper = new Swiper('.mySwiper', {    
-    slidesPreView: 1,
-    spaceBetween: 10,
-    loop: true,
-    Keyboard: {
-      enabled: true,
-    },  
+const swiper = new Swiper('.mySwiper', {
+  slidesPreView: 1,
+  spaceBetween: 10,
+  loop: true,
+  Keyboard: {
+    enabled: true,
+  },
 
-pagination: {
-      el: '.swiper-pagination',
-      type: 'custom',
-      clickable: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'custom',
+    clickable: true,
     renderCustom: function (swiper, current, total) {
       function numberAppend(d) {
-return (d < 10) ? '0' + d.toString() : d.toString();
-}
-  return numberAppend(current) + ' <span class="swiper-pagination-line"></span> ' + numberAppend(total); 
-}
+        return d < 10 ? '0' + d.toString() : d.toString();
+      }
+      return (
+        numberAppend(current) +
+        ' <span class="swiper-pagination-line"></span> ' +
+        numberAppend(total)
+      );
     },
+  },
 
-    navigation: {     
-      nextEl: '.swiper-btn-next',
-      prevEl: '.swiper-btn-prev',
-    },
-  });
+  navigation: {
+    nextEl: '.swiper-btn-next',
+    prevEl: '.swiper-btn-prev',
+  },
+});
